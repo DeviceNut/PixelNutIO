cb02a2ce215cbaa2431969aac0d2c25308b06c43
gdevalois <gdv@devicenut.com>
2021-11-07 14:52:49 -0800

    Remove revealer for trigging options, change button to text and add text for triggering force.

--- src/SectionTrigger.svelte
+++ src/SectionTrigger.svelte
@@ -1,8 +1,5 @@
 <script>
 
-  import CaretUp32 from "carbon-icons-svelte/lib/CaretUp32";
-  import CaretDown32 from "carbon-icons-svelte/lib/CaretDown32";
-
   import {
     Checkbox,
     Dropdown
@@ -51,123 +48,102 @@
   const setFtype   = () => { userSetForceType(  track, layer); }
   const setFvalue  = () => { userSetForceValue( track, layer); }
 
-  let isopen = true;
-
-  let bgc;
-  $: bgc = isopen ? '#222522' : '#222';
-
 </script>
 
-<div style="margin:5px 0 10px -10px; background-color: var(--bg-color-controls-area);">
+<div style="margin:5px 0 10px -10px; padding:0 5px 10px;
+            background-color: var(--bg-color-controls-area);">
 
-  <div style="cursor:pointer; margin-bottom:10px;
-              padding-top:7px; padding-bottom:10px;
-              background-color: {bgc};"
-       on:click={()=>{isopen = !isopen}}>
-    {#if isopen }
-      <CaretUp32 style="float:left; margin-left:10px;"/>
-    {:else}
-      <CaretDown32 style="float:left; margin-left:10px;"/>
-    {/if}
-    <button class="button"
-      > Triggering Options
-    </button>
-  </div>
+  <p style="padding:10px 0 10px 3px; font-size:.9em;">
+    Triggering Options:</p>
 
-  {#if isopen }
-    <div style="margin-left:10px; padding-bottom:8px;">
-      <Checkbox labelText="Once at start"
+  <Checkbox labelText="Once at start"
+    style="padding:3px;"
+    on:check={autoStart}
+    bind:checked={$pStrand.tracks[track].layers[layer].trigAtStart}
+  />
+  {#if !$pStrand.tracks[track].layers[layer].trigAtStart ||
+        ($pStrand.tracks[track].layers[layer].pluginBits & pluginBit_REPTRIGS) }
+    <Checkbox labelText="From main controls"
+      style="padding:3px;"
+      on:check={setMain}
+      bind:checked={$pStrand.tracks[track].layers[layer].trigFromMain}
+    />
+    {#if (($pStrand.trigSources).length > 1) }
+      <Checkbox labelText="From other effect"
         style="padding:3px;"
-        on:check={autoStart}
-        bind:checked={$pStrand.tracks[track].layers[layer].trigAtStart}
+        on:check={setOnLayer}
+        bind:checked={$pStrand.tracks[track].layers[layer].trigOnLayer}
       />
-      {#if !$pStrand.tracks[track].layers[layer].trigAtStart ||
-           ($pStrand.tracks[track].layers[layer].pluginBits & pluginBit_REPTRIGS) }
-        <Checkbox labelText="From main controls"
-          style="padding:3px;"
-          on:check={setMain}
-          bind:checked={$pStrand.tracks[track].layers[layer].trigFromMain}
+      {#if $pStrand.tracks[track].layers[layer].trigOnLayer }
+        <Dropdown
+          style="margin-bottom:10px;"
+          on:select={setSource}
+          bind:selectedIndex={$pStrand.tracks[track].layers[layer].trigSrcListDex}
+          bind:items={$pStrand.trigSources}
         />
-        {#if (($pStrand.trigSources).length > 1) }
-          <Checkbox labelText="From other effect"
-            style="padding:3px;"
-            on:check={setOnLayer}
-            bind:checked={$pStrand.tracks[track].layers[layer].trigOnLayer}
-          />
-          {#if $pStrand.tracks[track].layers[layer].trigOnLayer }
-            <Dropdown
-              style="margin-bottom:10px;"
-              on:select={setSource}
-              bind:selectedIndex={$pStrand.tracks[track].layers[layer].trigSrcListDex}
-              bind:items={$pStrand.trigSources}
-            />
-          {/if}
-        {/if}
-        <Checkbox labelText="Auto generated"
-          style="padding:3px;"
-          on:check={setRepeat}
-          bind:checked={$pStrand.tracks[track].layers[layer].trigDoRepeat}
-        />
-        {#if ($pStrand.tracks[track].layers[layer].trigDoRepeat) }
-          <div style="margin:12px 15px 0 15px;">
-            {#if ($pStrand.tracks[track].layers[layer].pluginBits & pluginBit_REPTRIGS) }
-              <span style="margin-right:9px">Repeat Count:&nbsp;&nbsp;&nbsp;</span>
-              <input type="number"
-                min=1 max=9999
-                on:change={setCount}
-                bind:value={$pStrand.tracks[track].layers[layer].trigRepCount}
-                disabled={$pStrand.tracks[track].layers[layer].trigForever}
-              />
-              <Checkbox labelText="Forever"
-                style="display:inline-block; margin-left:5px;"
-                on:check={setForever}
-                bind:checked={$pStrand.tracks[track].layers[layer].trigForever}
-              />
-            {/if}
-            <div style="margin-top:8px;">
-              <span style="margin-right:8px">Minimum Time:&nbsp;</span>
-              <input type="number"
-                min=1 max=9999
-                on:change={setOffset}
-                bind:value={$pStrand.tracks[track].layers[layer].trigRepOffset}
-              />&nbsp;&nbsp;secs
-            </div>
-            <div style="margin-top:8px; margin-bottom:10px;">
-              <span style="margin-right:8px">Random Period:</span>
-              <input type="number"
-                min=0 max=9999
-                on:change={setRange}
-                bind:value={$pStrand.tracks[track].layers[layer].trigRepRange}
-              />&nbsp;&nbsp;secs
-            </div>
-          </div>
-        {/if}
       {/if}
-      {#if ($pStrand.tracks[track].layers[layer].pluginBits & pluginBit_TRIGFORCE) &&
-           ($pStrand.tracks[track].layers[layer].trigAtStart ||
-            $pStrand.tracks[track].layers[layer].trigDoRepeat) }
-        <Checkbox labelText="Random force"
-          style="margin-left:5px; margin-top:20px;"
-          on:check={setFtype}
-          bind:checked={$pStrand.tracks[track].layers[layer].forceRandom}
-        />
-        <div style="margin-left:10px; margin-top:5px;">
-          <SliderVal name='Force:'
-            max={MAX_FORCE_VALUE}
-            onchange={setFvalue}
-            bind:cur={$pStrand.tracks[track].layers[layer].forceValue}
-            disabled={$pStrand.tracks[track].layers[layer].forceRandom} 
+    {/if}
+    <Checkbox labelText="Auto generated"
+      style="padding:3px;"
+      on:check={setRepeat}
+      bind:checked={$pStrand.tracks[track].layers[layer].trigDoRepeat}
+    />
+    {#if ($pStrand.tracks[track].layers[layer].trigDoRepeat) }
+      <div style="margin:12px 15px 0 15px;">
+        {#if ($pStrand.tracks[track].layers[layer].pluginBits & pluginBit_REPTRIGS) }
+          <span style="margin-right:9px">Repeat Count:&nbsp;&nbsp;&nbsp;</span>
+          <input type="number"
+            min=1 max=9999
+            on:change={setCount}
+            bind:value={$pStrand.tracks[track].layers[layer].trigRepCount}
+            disabled={$pStrand.tracks[track].layers[layer].trigForever}
           />
+          <Checkbox labelText="Forever"
+            style="display:inline-block; margin-left:5px;"
+            on:check={setForever}
+            bind:checked={$pStrand.tracks[track].layers[layer].trigForever}
+          />
+        {/if}
+        <div style="margin-top:8px;">
+          <span style="margin-right:8px">Minimum Time:&nbsp;</span>
+          <input type="number"
+            min=1 max=9999
+            on:change={setOffset}
+            bind:value={$pStrand.tracks[track].layers[layer].trigRepOffset}
+          />&nbsp;&nbsp;secs
         </div>
-      {/if}
+        <div style="margin-top:8px; margin-bottom:10px;">
+          <span style="margin-right:8px">Random Period:</span>
+          <input type="number"
+            min=0 max=9999
+            on:change={setRange}
+            bind:value={$pStrand.tracks[track].layers[layer].trigRepRange}
+          />&nbsp;&nbsp;secs
+        </div>
+      </div>
+    {/if}
+  {/if}
+
+  {#if ($pStrand.tracks[track].layers[layer].pluginBits & pluginBit_TRIGFORCE) &&
+        ($pStrand.tracks[track].layers[layer].trigAtStart ||
+        $pStrand.tracks[track].layers[layer].trigDoRepeat) }
+
+    <p style="padding:13px 0 5px 5px; font-size:.9em;">
+      Triggering Force:</p>
+
+    <Checkbox labelText="Random force"
+      style="margin-left:5px; margin-top:10px;"
+      on:check={setFtype}
+      bind:checked={$pStrand.tracks[track].layers[layer].forceRandom}
+    />
+    <div style="margin-left:10px; margin-top:-5px;">
+      <SliderVal name='Force:'
+        max={MAX_FORCE_VALUE}
+        onchange={setFvalue}
+        bind:cur={$pStrand.tracks[track].layers[layer].forceValue}
+        disabled={$pStrand.tracks[track].layers[layer].forceRandom} 
+      />
     </div>
   {/if}
-</div>
 
-<style>
-  .button {
-    border: none;
-    padding: 5px;
-    background-color: inherit;
-  }
-</style>
+</div>

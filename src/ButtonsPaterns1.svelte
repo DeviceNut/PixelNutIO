b37a018847aae6121937049533018b2a1cd00925
gdevalois <gdv@devicenut.com>
2021-11-06 18:20:29 -0700

    Same as previous commit.

---
+++ src/ButtonsPaterns1.svelte
@@ -0,0 +1,42 @@
+<script>
+
+  import { pStrand } from './globals.js';
+  import { userClearPattern } from './cmduser2.js';
+  import { sendEntirePattern } from './cmdsend.js';
+
+  const doclear   = () => { userClearPattern(); }
+  const dostore   = () => { sendEntirePattern(false); } // store don't execute
+  const dorestart = () => { sendEntirePattern(true); }  // store and execute
+
+</script>
+
+<button
+  class="button-pattern"
+  on:click={doclear}
+  disabled={ ($pStrand.tactives === 0) }
+  >Clear
+</button>
+
+<button
+  class="button-pattern"
+  on:click={dostore}
+  disabled={ ($pStrand.curPatternStr === '') }
+  >Store
+</button>
+
+<button
+  class="button-pattern"
+  on:click={dorestart}
+  disabled={ ($pStrand.curPatternStr === '') }
+  >Restart
+</button>
+
+<style>
+  .button-pattern {
+    width: 60px;
+    height: 40px;
+    padding: 5px;
+    margin-left: 7px;
+    margin-right: 7px;
+  }
+</style>
---
+++ src/ButtonsPaterns2.svelte
@@ -0,0 +1,162 @@
+<script>
+
+  import {
+    Modal,
+    Form,
+    FormGroup,
+    TextInput,
+    TextArea,
+    Checkbox,
+    Button,
+    ButtonSet
+  } from "carbon-components-svelte";
+
+  import {
+    pStrand,
+    aCurListPats,
+    updateSources,
+    storedPattern
+  } from './globals.js';
+
+  import {
+    storePatternsInit,
+    storePatternSave,
+    storePatternRemove
+  } from './browser.js';
+
+  import { userClearPattern } from './cmduser2.js';
+
+  let openSave = false;
+  let savedesc;
+  let copyclip = false;
+
+  const dosave = () =>
+  {
+    storePatternSave($pStrand.curPatternName, savedesc, $pStrand.curPatternStr);
+    storePatternsInit();
+
+    $updateSources = true;
+    $storedPattern = true;
+
+    if (copyclip)
+    {
+      copyToClipboard();
+      copyclip = false;
+    }
+
+    savedesc = '';
+    openSave = false;
+  }
+
+  let openDelete = false;
+  let remname;
+
+  const delstart = () =>
+  {
+    remname = $aCurListPats[$pStrand.curPatternIdx].text
+    openDelete = true;
+  }
+
+  const dodelete = () =>
+  {
+    storePatternRemove(remname);
+    storePatternsInit();
+    userClearPattern();
+
+    $updateSources = true;
+
+    openDelete = false;
+  }
+
+  function copyToClipboard()
+  {
+    let textArea = document.createElement("textarea");
+
+    textArea.value = $pStrand.curPatternStr;
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    let ok = true;
+    let errstr = '';
+    try
+    {
+      ok = document.execCommand('copy'); // FIXME: deprecated
+    }
+    catch (error)
+    {
+      errstr = error;
+      ok = false;
+    }
+    if (!ok) console.warn(`Failed copying to clipboard: ${errstr}`);
+
+    document.body.removeChild(textArea);
+  }
+
+</script>
+
+<button
+  class="button-pattern"
+  on:click={() => {openSave = !openSave;}}
+  disabled={$pStrand.curPatternStr === ''}
+  >Save
+</button>
+
+<button
+  class="button-pattern"
+  on:click={delstart}
+  disabled={!$pStrand.browserSource || ($pStrand.curPatternIdx === 0)}
+  >Delete
+</button>
+
+<Modal
+  passiveModal
+  modalHeading="Save Custom Pattern"
+  bind:open={openSave}
+  on:close
+  >
+  <Form on:submit={dosave} >
+    <FormGroup>
+      <TextInput
+        labelText="Name"
+        bind:value={$pStrand.curPatternName}
+      />
+      <div style="margin-top:10px;"></div>
+      <TextArea
+        labelText="Description"
+        bind:value={savedesc}
+      />
+    </FormGroup>
+    <Checkbox labelText="Copy command string to clipboard"
+      style="margin-top:-7px; margin-bottom:17px;"
+      bind:checked={copyclip}
+    />
+    <ButtonSet>
+      <Button kind="secondary" on:click={() => {openSave = false;}}>Cancel</Button>
+      <Button type="submit">Store</Button>
+    </ButtonSet>
+  </Form>
+</Modal>
+
+<Modal
+  passiveModal
+  modalHeading={`Delete Custom Pattern: "${remname}" ?`}
+  bind:open={openDelete}
+  on:close
+  >
+  <ButtonSet>
+    <Button kind="secondary" on:click={() => {openDelete = false;}}>Cancel</Button>
+    <Button on:click={dodelete}>Delete</Button>
+  </ButtonSet>
+</Modal>
+  
+<style>
+  .button-pattern {
+    width: 60px;
+    height: 40px;
+    padding: 5px;
+    margin-left: 7px;
+    margin-right: 7px;
+  }
+</style>

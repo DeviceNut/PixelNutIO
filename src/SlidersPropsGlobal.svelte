514c7f60f1942a4e7ab4663b1fb3f9462bacd5c8
gdevalois <gdv@devicenut.com>
2021-11-28 18:23:48 -0800

    Move override checkboxes to its own row, remove help button/field.

--- src/SlidersPropsGlobal.svelte
+++ src/SlidersPropsGlobal.svelte
@@ -26,36 +26,15 @@
   $: bgc = $pStrand.doOverride ? '#222' : '#111';
   // cannot use css vars here, and style cannot access globals
 
-  let helpon = false;
-
 </script>
 
 <div style="margin-top:15px; padding:10px; background-color: {bgc};">
 
-  <div style="margin-bottom:5px;">
-
-    <Checkbox labelText="Override Track Properties"
-      style="display:inline-block;"
-      on:check={userSetOverMode}
-      bind:checked={$pStrand.doOverride}
-      disabled={$pStrand.bitsOverride === 0}
-    />
-    <button class="button-help"
-      on:click={() => {helpon = !helpon;}}
-      >?
-    </button>
-
-    {#if helpon }
-      <div style="margin-top:5px; margin-bottom:5px; padding:5px;
-                  color: var(--color-textbox);
-                  background-color: var(--bg-color-textbox);">
-        <p style="font-size:.9em;">
-          Explain override track properties.
-        </p>
-      </div>
-    {/if}
-
-  </div>
+  <Checkbox labelText="Override Track Properties"
+    on:check={userSetOverMode}
+    bind:checked={$pStrand.doOverride}
+    disabled={$pStrand.bitsOverride === 0}
+  />
 
   <SliderVal name='Hue&nbsp;&nbsp;&nbsp;'
     onchange={userSetProps}
@@ -80,15 +59,3 @@
               !($pStrand.bitsEffects  & pluginBit_COUNT)}
   />
 </div>
-
-<style>
-  .button-help {
-    font-size: .8em;
-    width: 25px;
-    height: 25px;
-    padding: 3px;
-    margin-left: 20px;
-    border-width: 2px;
-    border-radius: 75%;
-  }
-</style>
--- src/SlidersPropsLocal.svelte
+++ src/SlidersPropsLocal.svelte
@@ -33,96 +33,61 @@
   const setcount = () => { userSetCount(track); }
   const setovers = () => { userSetOverrides(track); }
 
-  let helpon = false;
-
 </script>
 
-<div style="margin-left:-10px; margin-bottom:7px;
-            padding-top:13px; padding-bottom:8px;
-            background-color: var(--bg-color-controls-area);">
+<div style="margin: 20px -5px 10px -5px;
+            padding-top:15px; padding-bottom:10px;
+            background-color: var(--bg-color-controls-area);
+            ">
 
-  <div style="margin-left:-5px;">
-    <button class="button-help"
-      on:click={() => {helpon = !helpon;}}
-      >?
-    </button>
-    <p style="display:inline-block; margin-left:15px; font-size:.9em;">
-      Properties with overrides</p>
+  <p style="margin-left:10px; font-size:.9em">Property Overrides:</p>
 
-    {#if helpon }
-        <div style="margin:15px 10px 0 15px; padding:5px;
-                    color: var(--color-textbox);
-                    background-color: var(--bg-color-textbox);">
-          <p style="font-size:.9em;">
-            Explain main control overrides.
-          </p>
-        </div>
-    {/if}
-  </div>
-
+  <Row style="margin:10px 0 5px 10px; width:300px;">
+    <Checkbox labelText="Hue"
+      on:check={setovers}
+      bind:checked={$pStrand.tracks[track].drawProps.overHue}
+      disabled={!($pStrand.tracks[track].trackBits & pluginBit_COLOR) ||
+                  ($pStrand.tracks[track].trackBits & pluginBit_ORIDE_HUE)}
+    />
+    <Checkbox labelText="White"
+      on:check={setovers}
+      bind:checked={$pStrand.tracks[track].drawProps.overWhite}
+      disabled={!($pStrand.tracks[track].trackBits & pluginBit_COLOR) ||
+                  ($pStrand.tracks[track].trackBits & pluginBit_ORIDE_WHITE)}
+    />
+    <Checkbox labelText="Count"
+      on:check={setovers}
+      bind:checked={$pStrand.tracks[track].drawProps.overCount}
+      disabled={!($pStrand.tracks[track].trackBits & pluginBit_COUNT) ||
+                  ($pStrand.tracks[track].trackBits & pluginBit_ORIDE_COUNT)}
+    />
+  </Row>
   <Row>
     <Column style="margin-left:10px;">
-      <div style="max-width:245px;">
-        <SliderVal name='Hue&nbsp;&nbsp;&nbsp;'
-          max={359}
-          onchange={sethue}
-          bind:cur={$pStrand.tracks[track].drawProps.degreeHue}
-          disabled={($pStrand.doOverride && $pStrand.tracks[track].drawProps.overHue) ||
-                  !($pStrand.tracks[track].trackBits & pluginBit_COLOR)              ||
-                    ($pStrand.tracks[track].trackBits & pluginBit_ORIDE_HUE)}
-        />
-
-        <SliderVal name='White&nbsp;'
-          onchange={setwhite}
-          bind:cur={$pStrand.tracks[track].drawProps.pcentWhite}
-          disabled={($pStrand.doOverride && $pStrand.tracks[track].drawProps.overWhite) ||
-                  !($pStrand.tracks[track].trackBits & pluginBit_COLOR)                ||
-                    ($pStrand.tracks[track].trackBits & pluginBit_ORIDE_WHITE)}
-        />
-
-        <SliderVal name='Count&nbsp;'
-          onchange={setcount}
-          bind:cur={$pStrand.tracks[track].drawProps.pcentCount}
-          disabled={($pStrand.doOverride && $pStrand.tracks[track].drawProps.overCount) ||
-                  !($pStrand.tracks[track].trackBits & pluginBit_COUNT)                ||
-                    ($pStrand.tracks[track].trackBits & pluginBit_ORIDE_COUNT)}
-        />
-      </div>
-    </Column>
-    <Column>
-      <div style="margin-top:10px;"></div>
-      <Checkbox
-        on:check={setovers}
-        bind:checked={$pStrand.tracks[track].drawProps.overHue}
-        disabled={!($pStrand.tracks[track].trackBits & pluginBit_COLOR) ||
-                    ($pStrand.tracks[track].trackBits & pluginBit_ORIDE_HUE)}
+      <SliderVal name='Hue&nbsp;&nbsp;&nbsp;'
+        max={359}
+        onchange={sethue}
+        bind:cur={$pStrand.tracks[track].drawProps.degreeHue}
+        disabled={($pStrand.doOverride && $pStrand.tracks[track].drawProps.overHue) ||
+                 !($pStrand.tracks[track].trackBits & pluginBit_COLOR)              ||
+                  ($pStrand.tracks[track].trackBits & pluginBit_ORIDE_HUE)}
       />
-      <div style="margin-top:22px;"></div>
-      <Checkbox
-        on:check={setovers}
-        bind:checked={$pStrand.tracks[track].drawProps.overWhite}
-        disabled={!($pStrand.tracks[track].trackBits & pluginBit_COLOR) ||
-                    ($pStrand.tracks[track].trackBits & pluginBit_ORIDE_WHITE)}
+
+      <SliderVal name='White&nbsp;'
+        onchange={setwhite}
+        bind:cur={$pStrand.tracks[track].drawProps.pcentWhite}
+        disabled={($pStrand.doOverride && $pStrand.tracks[track].drawProps.overWhite) ||
+                 !($pStrand.tracks[track].trackBits & pluginBit_COLOR)                ||
+                  ($pStrand.tracks[track].trackBits & pluginBit_ORIDE_WHITE)}
       />
-      <div style="margin-top:20px;"></div>
-      <Checkbox
-        on:check={setovers}
-        bind:checked={$pStrand.tracks[track].drawProps.overCount}
-        disabled={!($pStrand.tracks[track].trackBits & pluginBit_COUNT) ||
-                    ($pStrand.tracks[track].trackBits & pluginBit_ORIDE_COUNT)}
+
+      <SliderVal name='Count&nbsp;'
+        onchange={setcount}
+        bind:cur={$pStrand.tracks[track].drawProps.pcentCount}
+        disabled={($pStrand.doOverride && $pStrand.tracks[track].drawProps.overCount) ||
+                 !($pStrand.tracks[track].trackBits & pluginBit_COUNT)                ||
+                  ($pStrand.tracks[track].trackBits & pluginBit_ORIDE_COUNT)}
       />
     </Column>
   </Row>
 </div>
-
-<style>
-  .button-help {
-    font-size: .8em;
-    width: 25px;
-    height: 25px;
-    padding: 3px;
-    margin-left: 15px;
-    border-width: 2px;
-    border-radius: 75%;
-  }
-</style>

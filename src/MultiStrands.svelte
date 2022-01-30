9da3b127245c530964ff90e41246345ebd65fee9
gdevalois <gdv@devicenut.com>
2022-01-29 16:13:49 -0800

    Put Strands all one line if only 2 or 3.

--- src/MultiStrands.svelte
+++ src/MultiStrands.svelte
@@ -22,20 +22,20 @@
   <div class="docenter">
     <div style="margin-top:15px;">
 
-      <MediaQuery query="(max-width: 700px)" let:matches>
-        {#if matches}
-          <span style="display:block; text-align:center;
-                       margin-bottom:10px; font-size:1.1em;"
-            >Strands:
-          </span>
-        {/if}
-      </MediaQuery>
-      <MediaQuery query="(min-width: 701px)" let:matches>
-        {#if matches}
-          <span style="font-size:1.1em;">Strands:</span>
-        {/if}
-      </MediaQuery>
-    
+      {#if ($nStrands > 3) }
+        <MediaQuery query="(max-width: 700px)" let:matches>
+          {#if matches}
+            <span class="spanblock">Strands:</span>
+          {/if}
+        </MediaQuery>
+        <MediaQuery query="(min-width: 701px)" let:matches>
+          {#if matches}
+            <span>Strands:</span>
+          {/if}
+        </MediaQuery>
+      {:else}
+        <span>Strands:</span>
+      {/if}
       {#each $aStrands as _,n}
         <Checkbox labelText={n+1}
           style="display:inline-block; margin-left:20px;"
@@ -60,6 +60,14 @@
   .docenter {
     display:flex;
     justify-content:center;
+  }
+  span {
+    font-size:1.1em;
+  }
+  .spanblock {
+    display: block;
+    text-align:center;
+    margin-bottom: 10px;
   }
   .divider {
     margin-top: 10px;

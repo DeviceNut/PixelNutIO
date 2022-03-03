2d87eaae02cadd4fb8e15aae3073fde6d5f7e620
gdevalois <gdv@devicenut.com>
2022-03-03 11:26:51 -0800

    Add more help placeholders.

--- src/helptext_1.js
+++ src/helptext_1.js
@@ -15,6 +15,16 @@ export const helpText_1 = (hmap) =>
 hmap.set(1001,
   "Help message 1001",
     );
+
+hmap.set(101,
+  "Help message 101",
+    );
+  
+  hmap.set(1010,
+"Help message 1010",
+  );
+
+hmap.set(1011,
+  "Help message 1011",
+    );
 }
-  
-  

8614c4a51063255408afa5258e4da3da6e0d750a
gdevalois <gdv@devicenut.com>
2022-03-27 12:57:47 -0700

    Previous commit broke it, this fixes it.

--- src/MultiStrands.svelte
+++ src/MultiStrands.svelte
@@ -47,7 +47,7 @@
 
       <Checkbox labelText='Dup'
         style="display:inline-block; margin-left:20px;"
-        on:check={()=> {userStrandCombine($dupStrand = !$dupStrand);}}
+        on:change={()=> {userStrandCombine($dupStrand);}}
         bind:checked={$dupStrand}
         />
 
